d2fde23eb81014068d4e6e037d82dca87b77e425
newmanuevers <seangnewman@msn.com>
2018-12-25 14:02:14 -0700

    Course completion

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sean = new Person("Sean", "Newman", 56);
+
+            Console.WriteLine($"Hello, {sean.FirstName}");
+
+            Assembly assem = Assembly.GetEntryAssembly();
+            Console.WriteLine(  assem.FullName);
+
+            Assembly people = sean.GetType().Assembly;
+            Console.WriteLine(people.FullName);
+        }
+    }
+}
---
+++ People/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(string fn, string ln, int a)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Age = a;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"[Person FN: {FirstName} LN:{LastName} A: {Age}]");
+        }
+    }
+}
